c63e94ffb6e110000721dbc99775ade084b0cccd
Bryan <bryanwong@student.csulb.edu>
2020-09-17 23:50:23 -0700

    snapshot 6

---
+++ js/ex3.js
@@ -0,0 +1,11 @@
+const num1 = Number(prompt("Enter first number"));
+const num2 = Number(prompt("Enter second number"));
+
+
+console.log ("The following mathematical operations for the number entered:");
+
+console.log (`${num1} + ${num2} =`, num1 + num2);
+console.log (`${num1} - ${num2} =`, num1 - num2);
+console.log (`${num1} * ${num2} =`, num1 * num2);
+console.log (`${num1} / ${num2} =`, num1 / num2);
+console.log (`${num1} % ${num2} =`, num1 % num2);
