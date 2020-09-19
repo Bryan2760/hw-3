28595b1e7ca406c171c838fadcf6da48607dc491
Bryan <bryanwong@student.csulb.edu>
2020-09-18 23:49:02 -0700

    snapshot9

--- js/ex6.js
+++ js/ex6.js
@@ -3,19 +3,20 @@ let min = prompt ("Enter minute");
 let sec = prompt ("Enter sec");
 
 console.log(`Time input:       ${hour}h ${min}m ${sec}s`);
-if (sec != 59){
+if (sec < 59){
+  sec++
   console.log(`One second later: ${hour}h ${min}m ${sec}s`);
-} else if (sec === "59" && min === "59" && hour === "23"){
+} else if (sec >= "59" && min >= "59" && hour >= "23"){
   hour = 0;
   min = 0;
   sec = 0;
   console.log(`One second later: ${hour}h ${min}m ${sec}s`);
-} else if (sec === "59" && min === "59"){
+} else if (sec >= "59" && min >= "59"){
   hour++;
   min = 0;
   sec = 0;
   console.log(`One second later: ${hour}h ${min}m ${sec}s`);
-} else if (sec === "59"){
+} else if (sec >= "59"){
   min++;
   sec = 0;
   console.log(`One second later: ${hour}h ${min}m ${sec}s`);
