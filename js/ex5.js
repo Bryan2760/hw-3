9fd6984427668148f2119729a341f7b74f5d9e83
Bryan <bryanwong@student.csulb.edu>
2020-09-18 23:27:56 -0700

    snapshot 8

---
+++ js/ex5.js
@@ -0,0 +1,7 @@
+const variable = prompt ("Enter a number to mulitply numbers 1-10");
+
+let x = 0;
+while (x < 11 ){
+  console.log(`${variable} * ${x} =`,variable * x);
+  x++
+}
---
+++ js/ex6.js
@@ -0,0 +1,26 @@
+let hour = prompt ("Enter hour : ");
+let min = prompt ("Enter minute");
+let sec = prompt ("Enter sec");
+
+console.log(`Time input:       ${hour}h ${min}m ${sec}s`);
+if (sec != 59){
+  console.log(`One second later: ${hour}h ${min}m ${sec}s`);
+} else if (sec === "59" && min === "59" && hour === "23"){
+  hour = 0;
+  min = 0;
+  sec = 0;
+  console.log(`One second later: ${hour}h ${min}m ${sec}s`);
+} else if (sec === "59" && min === "59"){
+  hour++;
+  min = 0;
+  sec = 0;
+  console.log(`One second later: ${hour}h ${min}m ${sec}s`);
+} else if (sec === "59"){
+  min++;
+  sec = 0;
+  console.log(`One second later: ${hour}h ${min}m ${sec}s`);
+}
+
+  
+
+
